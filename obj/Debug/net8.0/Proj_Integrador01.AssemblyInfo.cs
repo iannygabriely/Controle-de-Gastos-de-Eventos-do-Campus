dbd7375e71375090818b36dc4c2605bd4f01adbd
Vitor Veras <vitormanuelveras@gmail.com>
2024-03-23 16:16:23 -0300

    Adição da Controller e model do Usuario

--- obj/Debug/net8.0/Proj_Integrador01.AssemblyInfo.cs
+++ obj/Debug/net8.0/Proj_Integrador01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proj_Integrador01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afa6116742da746bb7a27675e387f0345ab8dfc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proj_Integrador01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proj_Integrador01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
